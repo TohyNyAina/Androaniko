cd0311722ebdc0ad0903f3fc023af7602a81ff77
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-09-09 09:03:19 +0300

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,21 +1,18 @@
+import Link from "next/link";
+
 export default function Register() {
     return (
-        <div>
+        <div className="bg-white">
             <div className="max-w-lg mx-auto  bg-white dark:bg-gray-800 rounded-lg shadow-md px-8 py-10 flex flex-col items-center">
-                <h1 className="text-xl font-bold text-center text-gray-700 dark:text-gray-200 mb-8">Welcome to My Company</h1>
+                <h1 className="text-xl font-bold text-center text-gray-700 dark:text-gray-200 mb-8">Bienvenu sur Androaniko</h1>
                 <form action="#" className="w-full flex flex-col gap-4">
                     <div className="flex items-start flex-col justify-start">
-                        <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">First Name:</label>
+                        <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Nom complet:</label>
                         <input type="text" id="firstName" name="firstName" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
                     </div>
 
                     <div className="flex items-start flex-col justify-start">
-                        <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Last Name:</label>
-                        <input type="text" id="lastName" name="lastName" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
-                    </div>
-
-                    <div className="flex items-start flex-col justify-start">
-                        <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Username:</label>
+                        <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Nom d' utilisateur:</label>
                         <input type="text" id="username" name="username" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
                     </div>
 
@@ -25,12 +22,12 @@ export default function Register() {
                     </div>
 
                     <div className="flex items-start flex-col justify-start">
-                        <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Password:</label>
+                        <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Mot de passe:</label>
                         <input type="password" id="password" name="password" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
                     </div>
 
                     <div className="flex items-start flex-col justify-start">
-                        <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Confirm Password:</label>
+                        <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Confirmation du mot de passe:</label>
                         <input type="password" id="confirmPassword" name="confirmPassword" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
                     </div>
 
@@ -38,8 +35,8 @@ export default function Register() {
                 </form>
 
                 <div className="mt-4 text-center">
-                    <span className="text-sm text-gray-500 dark:text-gray-300">Already have an account? </span>
-                    <a href="#" className="text-blue-500 hover:text-blue-600">Login</a>
+                    <span className="text-sm text-gray-500 dark:text-gray-300">Vous avez déjà un compte? </span>
+                    <Link href="/login" className="text-blue-500 hover:text-blue-600">Connexion</Link>
                 </div>
             </div>
         </div>
