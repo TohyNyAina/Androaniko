c90e92e8f3561791e13e088efac5bd94cd405e54
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-09-09 08:39:56 +0300

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ export default function Login () {
     return(
         <div>
             <h1>Login</h1>
+            <Link href="/register">Register</Link>
             <Link href="/welcome">Welcome</Link>
         </div>
     )
