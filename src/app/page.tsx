0e52175e9c81ee1a36e4673b36a4dfe7914cd0d6
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2024-09-09 09:46:02 +0300

    pwa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
+import { Html, Head, Main, NextScript } from "next/document";
 import Login from "./login/page";
 
 export default function Home() {
   return (
-    <div>
-      <Login></Login>
-    </div>
+    <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <meta name="theme-color" content="#000000"/>
+      </head>
+      <body>
+        <div>
+          <Login/>
+        </div>
+      </body>
+    </html>
   );
 }
